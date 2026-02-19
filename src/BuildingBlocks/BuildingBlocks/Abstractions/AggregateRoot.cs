8944452aa4a5cf118f768fb44cb32927582afbab
Emad.Ragheb <emaderagheb@gmail.com>
2026-02-19 13:44:00 +0200

    Add project files.

---
+++ src/BuildingBlocks/BuildingBlocks/Abstractions/AggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿namespace BuildingBlocks.Abstractions;
+
+public class AggregateRoot<TID> : Entity<TID>, IAggregateRoot<TID>
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+    public void AddDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
+    public IDomainEvent[] ClearDomainEvents()
+    {
+        var events = _domainEvents.ToArray();
+        _domainEvents.Clear();
+        return events;
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks/Abstractions/IAggregateRoot .cs
@@ -0,0 +1,9 @@
+﻿namespace BuildingBlocks.Abstractions;
+
+public interface IAggregateRoot:IEntity
+{
+    IReadOnlyCollection<IDomainEvent> DomainEvents { get; }
+    void AddDomainEvent(IDomainEvent domainEvent);
+    IDomainEvent[] ClearDomainEvents();
+}
+public interface IAggregateRoot<T> : IEntity<T> { }
